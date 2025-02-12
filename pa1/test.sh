426003fa32de4764c750aa5fa1d8027d1014e268
Daniel Li <inflmts@gmail.com>
2025-02-11 20:43:12 -0500

    pa1: finish

--- pa1/test.sh
+++ pa1/test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 set -ue
-space=' '
 ssh_command='set -e;
 rm -rf ~/workdir
 mkdir ~/workdir
 cd ~/workdir
+printf '\''\033[1m==> Extracting...\033[0m\n'\''
 tar -xvf-
 sh test.sh --emulator'
 
@@ -14,28 +14,30 @@ if [ "${1-}" != --emulator ]; then
   exit
 fi
 
-echo "==> Compiling..."
+printf '\033[1m==> Compiling...\033[0m\n'
 gcc -o pa1 -x assembler pa1.txt
 
 test_case() {
   local input="$1" result="$2"
   echo "** test: '$input'"
-  echo "$input" | ./pa1 | tee output
+  echo "$input" | stdbuf -oL ./pa1 | tee output
   if cmp -s - output <<EOF
-Input a string:$space
-String length: ${#input}
+Input a string: String length: ${#input}
 String is a palindrome (T/F): $result
 EOF
   then
-    echo "** pass"
+    printf '\033[1;32m** pass\033[0m\n'
   else
-    echo "** fail"
+    printf '\033[1;31m** fail\033[0m\n'
   fi
 }
 
-echo "==> Testing..."
-test_case racecar T
-test_case cannon F
-test_case Racecar F
-test_case r T
+printf '\033[1m==> Testing...\033[0m\n'
+test_case 'racecar' T
+test_case 'cannon' F
+test_case 'Racecar' F
+test_case 'r' T
 test_case '' T
+test_case 'catac' T
+test_case 'cattac' T
+test_case 'cataclysm' F
